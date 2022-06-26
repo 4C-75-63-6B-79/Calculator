61ef1b86a5f7725a1fabcce82e7c4b3bc6875c30
4C-75-63-6B-79 <luckyhairybeast987654@gmail.com>
2022-06-26 20:22:14 +0530

    Only four operators + - * / are working in the desired faishon
    = function need to be added into the calulator

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 // script.js
 
+let currentValue = '', previousValue = '' ;
+let currentSymbol = '', previousSymbol = '';
+
 function keyPressEvent(key) {
-    console.log(key.textContent);
+    mainCalculatorFunction(key.textContent.trim());
     key.classList.add('pressed');
 }
 
@@ -12,13 +15,55 @@ function endTransition(event){
     this.classList.remove('pressed');
 }
 
-const keys = Array.from(document.querySelectorAll('.button'));
-keys.forEach((key) => key.addEventListener('click', () => keyPressEvent(key)));
-keys.forEach((key) => key.addEventListener('transitionend', endTransition));
+function start() {
+    const keys = Array.from(document.querySelectorAll('.button'));
+    keys.forEach((key) => key.addEventListener('click', () => keyPressEvent(key)));
+    keys.forEach((key) => key.addEventListener('transitionend', endTransition));
 
-window.addEventListener('keydown', (event) => {
-    const key = document.querySelector(`div[data-code="${event.code}"]`);
-    if(key) {
-        keyPressEvent(key);
+    window.addEventListener('keydown', (event) => {
+        const key = document.querySelector(`div[data-code="${event.code}"]`);
+        if(key) {
+            keyPressEvent(key);
+        }
+    });
+}
+
+function mainCalculatorFunction(input) {
+    if(Number(input) >=0 && Number(input) <= 9) {
+        currentValue += input;
+        document.querySelector('#line2').textContent =  currentValue;
     }
-})
+    if(input == '+' || input == '-' || input == '*' || input == '/') {
+        if(currentSymbol == '') {
+            currentSymbol = input;
+            previousValue = currentValue;
+            currentValue = '';
+            document.querySelector('#line1').textContent = previousValue + currentSymbol;
+            document.querySelector('#line2').textContent =  currentValue;            
+        } else {
+            previousSymbol = currentSymbol;
+            currentSymbol = input;
+            solve();
+            currentValue = '';
+            document.querySelector('#line1').textContent = previousValue + currentSymbol;
+            document.querySelector('#line2').textContent =  currentValue; 
+        }
+    }
+}
+
+function solve() {
+    if(previousSymbol == '+') {
+        previousValue = Number(previousValue) + Number(currentValue);
+    } else if(previousSymbol == '-') {
+        previousValue = Number(previousValue) - Number(currentValue);
+    } else if(previousSymbol == '*') {
+        previousValue = Number(previousValue) * Number(currentValue);
+    } else if(previousSymbol == '/') {
+        previousValue = Number(previousValue) / Number(currentValue);
+    }
+}
+
+
+
+
+start();
