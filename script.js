ff1404e6a5a3fc5337964b1963e9c9df639b8371
4C-75-63-6B-79 <luckyhairybeast987654@gmail.com>
2022-06-27 13:03:16 +0530

    the calculator work with the operation buttons but
    The problem is that the way the mainCalculator function is written is terrible way to do the things and I am sorry

--- script.js
+++ script.js
@@ -28,28 +28,48 @@ function start() {
     });
 }
 
+// This is a terrible way to write a caluclator function and I am sorry as I was not able to make the thing work using functions.
 function mainCalculatorFunction(input) {
-    if(Number(input) >=0 && Number(input) <= 9) {
+    if(Number(input)>=0 && Number(input)<=9 || input == '.') {
+        if(currentValue.includes('.') && input == '.') {
+            return;
+        }
         currentValue += input;
-        document.querySelector('#line2').textContent =  currentValue;
-    }
-    if(input == '+' || input == '-' || input == '*' || input == '/') {
-        if(currentSymbol == '') {
+        document.querySelector('#line2').textContent = currentValue;
+    } else if(input == '+' || input == '-' || input == '*' || input == '/'){
+        if(currentSymbol == '' && previousSymbol == '') {
             currentSymbol = input;
-            previousValue = currentValue;
+            previousValue += currentValue;
             currentValue = '';
             document.querySelector('#line1').textContent = previousValue + currentSymbol;
-            document.querySelector('#line2').textContent =  currentValue;            
+            document.querySelector('#line2').textContent = currentValue;
+        } else if(currentSymbol == '=') {
+            currentSymbol = input;
+            previousSymbol = currentSymbol;
+            document.querySelector('#line1').textContent = previousValue + currentSymbol;
+            currentValue = '';
+            document.querySelector('#line2').textContent = currentValue;
         } else {
             previousSymbol = currentSymbol;
             currentSymbol = input;
             solve();
-            currentValue = '';
             document.querySelector('#line1').textContent = previousValue + currentSymbol;
-            document.querySelector('#line2').textContent =  currentValue; 
+            currentValue = '';
+            document.querySelector('#line2').textContent = currentValue;
+        } 
+    } else if(input == '=') {
+        if(previousValue != '') {
+            previousSymbol = currentSymbol;
+            currentSymbol = input;
+            document.querySelector('#line1').textContent = previousValue + previousSymbol + currentValue + currentSymbol;
+            solve();
+            document.querySelector('#line2').textContent = previousValue;
+            currentValue = '';
         }
     }
+
 }
+
 
 function solve() {
     if(previousSymbol == '+') {
